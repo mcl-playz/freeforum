61a6e52da2d0ba3ef18cd012c9df1745ef7bdcd7
mcl-playz <elvi.green11@gmail.com>
2025-10-05 20:17:08 +0800

    Updated button styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,19 +7,19 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
-	<title></title>
+	<title>FreeForum</title>
 </svelte:head>
 
 <div class="bg-brand-dark pt-4 h-screen">
-	<div class="mt-0 box-rounded flex items-center justify-between p-3">
+	<div class="mt-0 box-rounded flex items-center justify-between p-3 relative">
 		<div class="flex items-center space-x-4 ml-3">
 			<a class="hover:underline" href="/">Home</a>
 			<a class="hover:underline" href="/test">Test</a>
 		</div>
-		<h1 class="text-center flex-1 font-bold">FreeForum</h1>
+		<h1 class="absolute left-1/2 transform -translate-x-1/2 font-bold">FreeForum</h1>
 		<div class="flex items-center space-x-2">
-			<button class="btn" onclick={() => {document.location.href="/signin"}}>Login</button>
-			<button class="btn" onclick={() => {document.location.href="/signup"}}>Sign Up</button>
+			<button class="muted" onclick={() => {document.location.href="/signin"}}>Login</button>
+			<button class="muted" onclick={() => {document.location.href="/signup"}}>Sign Up</button>
 		</div>
 	</div>
 	<div>
